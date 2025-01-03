9178c25c2013b4f7355a2fd74a139a143fa2d39f
Scharly <scharly.opitz@gmail.com>
2025-01-03 13:36:06 +0100

    Ajout background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Background from "./components/Background";
 import Projets from "./components/Projets";
 
 export default function App() {
   return (
     <main>
+      <Background />
       <Projets />
     </main>
   );

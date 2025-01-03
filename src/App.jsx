cc14f4d0411f09513d6b545c6f109c25f8635267
Scharly <scharly.opitz@gmail.com>
2025-01-03 13:26:17 +0100

    Ajout projet

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Projets from "../src/assets/Projets.json";
 export default function App() {
   return (
     <main>
-      {/* {Projets.map((projet,i)=>{
-      return <div key={i} className="projet" style={{w}}></div>
-    })} */}
+      {Projets.map((projet, i) => {
+        return (
+          <div key={i} className="image">
+            <img src={projet.image} alt={projet.name} />
+          </div>
+        );
+      })}
     </main>
   );
 }

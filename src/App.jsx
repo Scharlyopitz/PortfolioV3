7a2dca9bd160e6ab435b19cedad6d50577abbe4f
Scharly <scharly.opitz@gmail.com>
2025-01-03 13:20:30 +0100

    Ajout data des projets

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
+import Projets from "../src/assets/Projets.json";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      {/* {Projets.map((projet,i)=>{
+      return <div key={i} className="projet" style={{w}}></div>
+    })} */}
+    </main>
   );
 }
-
-export default App;

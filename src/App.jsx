d5763b6462ed398084bf1ed7e47f420361b21903
Scharly <scharly.opitz@gmail.com>
2025-01-03 14:13:59 +0100

    Ajout lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import Background from "./components/Background";
 import Projets from "./components/Projets";
+import Lenis from "lenis";
 
 export default function App() {
+  useEffect(() => {
+    // RESET DE L'HISTORIQUE DE L'URL ET SCROLLRESTORATION POUR SCROLL TO TOP
+    history.scrollRestoration = "manual";
+
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main>
       <Background />

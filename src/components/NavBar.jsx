3384e066a6728183d12844c009a34e7763137a8c
Scharly <scharly.opitz@gmail.com>
2025-01-06 14:00:38 +0100

    Supression du boutton a propos sur la page a propos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "/Logo.png";
 
 export default function NavBar() {
+  const { pathname } = useLocation();
   return (
     <nav>
       <Link to="/" className="logoContainer">
         <img src={Logo} alt="logo" />
       </Link>
-      <Link to="/apropos" className="AboutBtn">
-        A propos
-      </Link>
+      {pathname !== "/apropos" && (
+        <Link to="/apropos" className="AboutBtn">
+          A propos
+        </Link>
+      )}
     </nav>
   );
 }

59aba8f176ae71abf73ecf9b09af26932420ba73
Scharly <scharly.opitz@gmail.com>
2025-01-06 14:18:05 +0100

    Mise au propre du code et ajout feuille de style page apropos

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -2,7 +2,7 @@ import AproposData from "../assets/AproposData.json";
 
 export default function Apropos() {
   return (
-    <main>
+    <main id="Apropos">
       <div className="descriptionContainer">
         {AproposData[0].split(" ").map((word, i) => {
           return <p key={i}>{word}</p>;

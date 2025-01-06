a427dd7974bd1a3929fb9155dcd149ba3ad00bca
Scharly <scharly.opitz@gmail.com>
2025-01-06 10:42:30 +0100

    Mise en page de la page projet

--- src/pages/Projet.jsx
+++ src/pages/Projet.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Projets from "../assets/Projets.json";
 import { useParams } from "react-router-dom";
+import Background from "../components/Background";
 
 export default function Projet() {
   const { projet } = useParams();
 
   const rightProject = Projets.find((p) => p.link === projet);
 
-  return <div>Bienvenue sur le projet {rightProject.name}</div>;
+  return (
+    <main>
+      <Background />
+      <h1>Bienvenue sur le projet {rightProject.name}</h1>
+    </main>
+  );
 }

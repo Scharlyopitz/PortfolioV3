6f03ca6eb7ff6c6f49388918425ab4e993235149
Scharly <scharly.opitz@gmail.com>
2025-01-07 12:19:35 +0100

    Animation description

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,10 +1,31 @@
 import { Link } from "react-router-dom";
 import AproposData from "../assets/AproposData.json";
 import TitlePage from "../components/TitlePage";
+import { motion as m } from "motion/react";
 
 export default function Apropos() {
   const githubLink = "https://github.com/Scharlyopitz";
   const instagramLink = "https://www.instagram.com/s.optz/?hl=fr";
+
+  const easeOutCubic = [0.33, 1, 0.68, 1];
+
+  const descriptionAnime = {
+    initial: {
+      y: "105%",
+    },
+    animate: (i) => ({
+      y: "0",
+      transition: {
+        duration: 0.75,
+        delay: 0.007 * i,
+        ease: easeOutCubic,
+      },
+    }),
+    exit: {
+      y: "105%",
+      transition: { duration: 0.75, ease: [0.65, 0, 0.35, 1] },
+    },
+  };
   return (
     <main id="Apropos">
       <TitlePage title="A propos" />
@@ -12,7 +33,15 @@ export default function Apropos() {
         {AproposData[0].split(" ").map((word, i) => {
           return (
             <div key={i} className="hidden">
-              <p>{word}</p>
+              <m.p
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                custom={i}
+                variants={descriptionAnime}
+              >
+                {word}
+              </m.p>
             </div>
           );
         })}

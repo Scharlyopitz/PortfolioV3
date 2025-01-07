29def7052ade3a6a0114c0dfdb2501de67d135b8
Scharly <scharly.opitz@gmail.com>
2025-01-07 11:47:06 +0100

    Ajout span pour animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,34 @@
 import { Link, useLocation } from "react-router-dom";
 import Logo from "/Logo.png";
+import { AnimatePresence } from "motion/react";
 
 export default function NavBar() {
   const { pathname } = useLocation();
+
+  // const buttonAnimation = {
+  //   initial:{
+
+  //   },
+  //   animate:{
+
+  //   },
+  //   exit:{
+
+  //   },
+  // }
+
   return (
     <nav>
       <Link to="/" className="logoContainer">
         <img src={Logo} alt="logo" />
       </Link>
-      {pathname !== "/apropos" && (
-        <Link to="/apropos" className="AboutBtn">
-          A propos
-        </Link>
-      )}
+      <AnimatePresence>
+        {pathname !== "/apropos" && (
+          <Link to="/apropos" className="AboutBtn">
+            <span>A propos</span>
+          </Link>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }

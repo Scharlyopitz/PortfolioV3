3760c716359b0c8830951cb934aff19f2d150e8e
Scharly <scharly.opitz@gmail.com>
2025-01-10 14:55:58 +0100

    Mise à jour titre du site

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 
 export default function TitlePage({ title }) {
   useEffect(() => {
-    document.title = `Scharly Opitz | ${title}`;
+    document.title = `Scharly Opitz — ${title}`;
   }, []);
 }
